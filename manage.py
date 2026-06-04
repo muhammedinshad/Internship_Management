6f261e6bda83025540dd0c55227e34a7005b12f9
muhammedinshad30-web <muhammedinshad30@gmail.com>
2026-06-04 12:25:39 +0530

    Initial project setup

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'internship_platform.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'internship_platform.settings.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
